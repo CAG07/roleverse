b826c59cffb8c201f96c1852e74341125a5164a9
Craig Gehrig <craig.gehrig@gmail.com>
2026-02-13 14:03:18 -0600

    Options syntax fix

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -18,11 +18,11 @@ export async function middleware(request: NextRequest) {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, _options }) => request.cookies.set(name, value));
+          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           supabaseResponse = NextResponse.next({
             request,
           });
-          cookiesToSet.forEach(({ name, value, _options }) =>
+          cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
           );
         },
